0c875ed5575b416ec2c42567a298b4fb94c55a8e
joshh450 <joshh450@yahoo.com>
2018-10-12 11:21:53 -0600

    set up our links in the header and created the components that we need for the project, updated parallax component

---
+++ src/components/parallax.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Parallax extends Component {
+    render() {
+        return(
+            <div className="parallax"></div>
+        )
+    }
+}
+export default Parallax;
