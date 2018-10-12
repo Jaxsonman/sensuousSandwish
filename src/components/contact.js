7a7ec95f449abfb027fc28af8d7c46876153aedf
joshh450 <joshh450@yahoo.com>
2018-10-12 14:53:18 -0600

    fixed the map feature on the contact page to take in props and render the correct map location for Orem or Provo

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,9 +9,9 @@ class Contact extends Component {
         return(
             <div>
                 <Header/>
-                <Map/>
+                <Map location="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194665.11702719072!2d-111.91840885223054!3d40.32809211241413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d975494ad288d%3A0xe55b532ce8a0eee4!2sSensuous+Sandwich!5e0!3m2!1sen!2sus!4v1539376979408"/>
                 <ContactMessage/>
-                <Map/>
+                <Map location="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75568.226598349!2d-111.73947403033411!3d40.257610683412146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc1e6346e076c8724!2sSensuous+Sandwich!5e0!3m2!1sen!2sus!4v1539377496989"/>
                 <ContactMessage/>
                 <Footer/>
             </div>
