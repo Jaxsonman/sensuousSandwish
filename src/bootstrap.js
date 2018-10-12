26bcabc343eadcdbf7d677cccef9cc9570c6c150
eidanfili <eidan@ledezma.org>
2018-10-12 12:01:30 -0600

    commit the code

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './components/app';
+// import Menu from './components/menu';
+// import Contact from './components/contact';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -16,7 +18,11 @@ function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Switch>
+          <Route exact path="/" component={App} />
+          {/* <Route path="/menu" component={Menu} />
+          <Route path="/contact" component={Contact} /> */}
+        </Switch>
       </BrowserRouter>
     </Provider>
     , document.querySelector('.app-wrapper'));
--- src/components/app.js
+++ src/components/app.js
@@ -9,9 +9,9 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Header/>
+        {/* <Header/>
         <Parallax/>
-        <Description/>
+        <Description/> */}
         <Footer/>
       </div>
     );
