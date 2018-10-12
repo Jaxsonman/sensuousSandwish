4cdaa03de81cb067d590fb9b7e62e595a60ac01a
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-12 13:44:25 -0600

    bug

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Header from './header-footer/header';
 import Footer from './header-footer/footer';
-import Parallax from './parallax';
+// import Parallax from './parallax';
 import Description from './description';
 
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className='app'>
+
         <Header/>
-        <Parallax/>
-        <Description/>
-        <Footer/>
+        <img src="C:\Users\jaxbl\OneDrive\Desktop\Bottega\PersonalProjects\sensuousSandwich2\sensuousSandwish\static\assets\images\paralax.jpg"/>
+        {/* <Parallax/> */}
+        {/* <Description/>
+        <Footer/> */}
       </div>
     );
   }
 }
+
+export default App;
