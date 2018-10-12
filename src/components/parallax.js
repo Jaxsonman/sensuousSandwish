32dc142e266612cd115054c0b35ec3020e7570bd
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-12 13:35:19 -0600

    header styling finished

--- src/components/parallax.js
+++ src/components/parallax.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { ParallaxProvider } from 'react-scroll-parallax';
+
 
 class Parallax extends Component {
     render() {
         return(
-            <div className="parallax"></div>
+            <div className="parallax__wrapper">
+                <img className="parallax" src="/assets/images/paralax.png" />
+            </div>
         )
     }
 }
